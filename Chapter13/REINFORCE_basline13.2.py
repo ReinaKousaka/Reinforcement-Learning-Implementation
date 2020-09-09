5440c7d925c05cb598f4bc50904c7c428063a5b6
Songheng <yinsongheng@gmail.com>
2020-09-09 16:26:16 -0400

    add 13.2

--- Chapter13/REINFORCE_basline13.2.py
+++ Chapter13/REINFORCE_basline13.2.py
@@ -28,7 +28,7 @@ class Env:
 		else:
 			return self.state, -1, False
 
-class Agent:
+class REINFORCE_Agent:
 	def __init__(self, env, lr, gamma=1):
 		self.env = env
 		self.lr = lr
@@ -79,21 +79,55 @@ class Agent:
 				y[_] += total_reward
 		y = y / n_epochs
 		return y
-		
+
+class REINFORCE_Basline_Agent(REINFORCE_Agent):
+	def __init__(self, env, alpha_theta, alpha_w, gamma=1):
+		super(REINFORCE_Basline_Agent, self).__init__(env, alpha_theta, gamma)
+		self.w = 0  # The state-value weight vector is dim-1 in this question!
+		self.alpha_w = alpha_w
+	
+	def reset(self):
+		self.theta = torch.tensor([-1.47, 1.47])
+		self.w = 0
+
+	def learn(self):
+		n_episodes = 1000
+		n_epochs = 50
+		y = np.zeros(n_episodes)
+		for epoch in tqdm(range(n_epochs)):
+			self.reset()
+			for _ in range(n_episodes):
+				trajectory = self.get_episode()
+				G = 0
+				total_reward = 0
+				for state, action, reward in reversed(trajectory):
+					G = G * self.gamma + reward
+					total_reward += reward
+					delta = G - self.w
+					self.w = self.w + self.alpha_w * delta * 1  # the grad of the baseline is 1
+					
+					h = torch.matmul(X, self.theta)
+					pi = torch.softmax(h, dim=0)
+					grad_ln_action = X[action] - (pi[0].item() * X[0] + pi[1].item() * X[1])
+					self.theta += self.lr * delta * grad_ln_action
+					
+				y[_] += total_reward
+		return y / n_epochs
+				
+				
 
 if __name__ == '__main__':
 	env = Env()
-	agent1 = Agent(env, 2e-4)
+	agent1 = REINFORCE_Agent(env, 2e-4)
 	y1 = agent1.learn()
-	agent2 = Agent(env, 2e-5)
+	agent2 = REINFORCE_Basline_Agent(env, 2e-3, alpha_w=2e-2)
 	y2 = agent2.learn()
-	agent3 = Agent(env, 2e-3)
-	y3 = agent1.learn()
+	
 	x = np.arange(1000) + 1
 	fig, ax = plt.subplots(1, 1)
-	ax.plot(x, y1, label='2e-4')
-	ax.plot(x, y2, label='2e-5')
-	ax.plot(x, y3, label='2e-3')
+	ax.plot(x, y1, label='REINFORCE')
+	ax.plot(x, y2, label='REINFORCE with basline')
+
 	ax.set_xlabel('Episode')
 	ax.set_ylabel('Total reward on episode')
 	ax.legend()
