0b1736a8fffc2d327d58bcb0cd4d7d308af20e45
Songheng <yinsongheng@gmail.com>
2020-09-11 16:13:37 -0400

    DQN_CartPole works

--- DQN/DQN_CartPole.py
+++ DQN/DQN_CartPole.py
@@ -8,6 +8,10 @@ import random
 import numpy as np
 from collections import namedtuple
 from matplotlib import pyplot as plt
+import sys
+
+sys.setrecursionlimit(3000)
+
 
 class Net(nn.Module):
 	def __init__(self, input_dims, num_actions):
@@ -35,8 +39,6 @@ class DQNAgent:
 		self.Q_eval = Net(num_actions=env.action_space.n, input_dims=env.observation_space.shape[0])
 		self.optimizer = torch.optim.Adam(self.Q_eval.parameters(), lr=lr)  # optimizer
 		self.Q_target = Net(num_actions=env.action_space.n, input_dims=env.observation_space.shape[0])
-		self.Q_target.load_state_dict(self.Q_eval.state_dict())
-		self.Q_target.eval()
 
 	def choose_action(self, state):
 		if random.random() < self.epsilon:
@@ -48,58 +50,68 @@ class DQNAgent:
 	
 	def update_model(self):
 		samples = self.memory.sample(self.batch_size)
-		loss = self._compute_loss(self, samples)
+		loss = self._compute_loss(samples)
 		self.optimizer.zero_grad()
 		loss.backward()
 		self.optimizer.step()
 		return loss.item()
 	
 	def _compute_loss(self, samples):
-		states, actions, rewards, next_states, dones = samples
-		states = torch.FloatTensor(states)
-		actions = torch.FloatTensor(actions)
-		rewards = torch.FloatTensor(rewards)
-		next_states = torch.FloatTensor(next_states)
-		dones = torch.FloatTensor(dones)
-
-		curr_q_value = self.Q_eval(states).gather(1, action)
-		next_q_value = self.Q_target(next_states).max(dim=1, keepdim=True)[0].detach()
-		mask = 1 - done
-		target = (rewards + self.gamma * next_q_value * mask)
-		loss = torch.nn.MSELoss(target, curr_q_value)
+		target_list = torch.zeros(len(samples))
+		pred_list = torch.zeros(len(samples))
+		for i, (state, action, reward, next_state, done) in enumerate(samples):
+			if done:
+				target_list[i] = reward
+			else:
+				self.Q_target.load_state_dict(self.Q_eval.state_dict())
+				temp = self.Q_target(torch.from_numpy(next_state).float()).detach()
+				target_list[i] = reward + self.gamma * torch.max(temp)
+			pred_list[i] = self.Q_eval(torch.from_numpy(state).float())[action] 
+		loss = torch.nn.MSELoss()(target_list, pred_list)  
 		return loss
 	
-	def train(self):
-		num_episodes = 100
+	def train(self, time_step=2000):
 		state = self.env.reset()
 		score = 0
 		scores_lst = []
 		losses_lst = []
-		for _ in range(num_episodes):
-			self.env.render()
-
+		for _ in range(time_step):
+			#self.env.render()
 			action = self.choose_action(state)
 			next_state, reward, done, info = self.env.step(action)
 			self.memory.push(state, action, reward, next_state, done)
 
+			if len(self.memory) >= self.batch_size:  # if we have enough replays
+				loss = self.update_model()
+				losses_lst.append(loss)
 			score += reward
 			state = next_state
 			if done:
 				scores_lst.append(score)
 				score = 0
 				state = self.env.reset()
-				continue
-			if len(self.memory) >= self.batch_size:  # if we have enough replays
-				loss = self.update_model()
-				losses_lst.append(loss)
+			
 		self.env.close()
 		return scores_lst, losses_lst
-
+	
+	def test(self, render):
+		state = self.env.reset()
+		done = False
+		score = 0
+		while not done:
+			if render:
+				self.env.render()
+			action = self.choose_action(state)
+			next_state, reward, done, info = self.env.step(action)
+			state = next_state
+			score += reward
+		self.env.close()
+		return score
 
 
 
 class ReplayBuffer:
-	def __init__(self, buffer_size=100000):
+	def __init__(self, buffer_size=10000):
 		self.buffer_size = buffer_size
 		self.buffer = []
 		self.index_pointer = 0
@@ -112,17 +124,8 @@ class ReplayBuffer:
 			self.index_pointer = (self.index_pointer + 1) % self.buffer_size
 
 	def sample(self, batch_size):
-		assert batch_size <= len(self.buffer)
-		states, actions, rewards, next_states, dones = [], [], [], [], []
-		for _ in range(batch_size):
-			i = random.randint(0, len(self.buffer_size) - 1)
-			state, action, reward, next_state, done = self.buffer[i]
-			states.append(state)
-			actions.append(action)
-			rewards.append(reward)
-			next_states.append(next_state)
-			dones.append(done)
-		return (states, actions, rewards, next_states, dones)
+		samples = random.sample(self.buffer, batch_size)
+		return samples
 	
 	def __len__(self):
 		return len(self.buffer)
@@ -130,8 +133,17 @@ class ReplayBuffer:
 
 if __name__ == "__main__":
 	env = gym.make('CartPole-v0')
-	agent = DQNAgent(env=env, )
+	agent = DQNAgent(env=env)
+	print('The score before training: {}'.format(agent.test(render=False)))
 	score, loss = agent.train()
-	fig, ax = plt.subplots(1, 1)
-	ax.plot(np.arange(len(loss)), loss)
+	print('The score after training: {}'.format(agent.test(render=True)))
+	
+
+	# Draw
+	fig, (ax1, ax2) = plt.subplots(1, 2)
+	ax1.plot(np.arange(len(loss)), loss)
+	ax1.set_ylabel('Loss')
+	ax2.plot(np.arange(len(score)), score)
+	ax2.set_ylabel('Score')
+	fig.tight_layout()
 	plt.show()
